23365880207091788900d5e2a08441f7cd68eb01
Sujin Kang <elvin9792@gmail.com>
2023-12-07 20:24:33 -0800

    Day 3

--- day3/problem1.py
+++ day3/problem1.py
@@ -1,5 +1,67 @@
 """Advent of code"""
 
-with open('sample.txt', 'r') as fin:
+from collections import deque
+
+DIRS = ((-1, -1), (-1, 0), (-1, 1),
+        (0, -1),           (0, 1),
+        (1, -1),  (1, 0),  (1, 1))
+
+grid = []
+
+with open('input.txt', 'r', encoding='utf-8') as fin:
     for line in fin:
-        pass
+        grid.append(line.strip())
+
+
+def get_number_and_mark_visited(grid, r, c, visited):
+    """Get the number at the location, and mark starting position as visited."""
+    q = deque()
+
+    col = c
+    while 0 <= col and grid[r][col].isnumeric():
+        q.appendleft(grid[r][col])
+        col -= 1
+
+    if (r, col) in visited:
+        return 0
+
+    visited.add((r, col))
+
+    col = c + 1
+    while col < len(grid[0]) and grid[r][col].isnumeric():
+        q.append(grid[r][col])
+        col += 1
+
+    return int("".join(q))
+
+
+def get_sum_of_near_nums(grid, i, j, visited):
+    """
+    Given a cell position, sum all numbers adjacent to the cell 
+    and get summation of them if number has not be visited.
+
+    visited will contain start position of the cell where the number starts.
+    """
+    total = 0
+    for x_delta, y_delta in DIRS:
+        if grid[i + x_delta][j + y_delta].isnumeric():
+            total += get_number_and_mark_visited(grid, i + x_delta, j + y_delta, visited)
+
+    return total
+
+
+def get_total(grid):
+    """Get total summation of engine parts"""
+    total = 0
+
+    visited = set()
+
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if not grid[i][j].isnumeric() and grid[i][j] != '.':  # if special symbol
+                total += get_sum_of_near_nums(grid, i, j, visited)
+
+    return total
+
+if __name__ == "__main__":
+    print(get_total(grid))
--- day3/problem2.py
+++ day3/problem2.py
@@ -1,5 +1,73 @@
 """Advent of code"""
 
-with open('sample.txt', 'r') as fin:
+from collections import deque
+
+DIRS = ((-1, -1), (-1, 0), (-1, 1),
+        (0, -1),           (0, 1),
+        (1, -1),  (1, 0),  (1, 1))
+
+
+def get_number_at(grid, r, c, visited):
+    """Get the number at the location, and mark starting position as visited."""
+    q = deque()
+
+    col = c
+    while 0 <= col and grid[r][col].isnumeric():
+        q.appendleft(grid[r][col])
+        col -= 1
+
+    if (r, col) in visited:
+        return 0
+
+    visited.add((r, col))
+
+    col = c + 1
+    while col < len(grid[0]) and grid[r][col].isnumeric():
+        q.append(grid[r][col])
+        col += 1
+
+    return int("".join(q))
+
+
+def get_sum_of_gear_ratios(grid, i, j):
+    """
+    Given a cell position, sum all numbers adjacent to the cell 
+    and get summation of them if number has not be visited.
+
+    visited will contain start position of the cell where the number starts.
+    """
+    nums = []
+
+    visited = set()
+
+    for x_delta, y_delta in DIRS:
+        if grid[i + x_delta][j + y_delta].isnumeric():
+            number = get_number_at(grid, i + x_delta, j + y_delta, visited)
+            if number > 0:
+                nums.append(number)
+
+    if len(nums) == 2:
+        return nums[0] * nums[1]
+
+    return 0
+
+
+def get_total_gear_ratio(grid):
+    """Get total summation of engine parts"""
+    total = 0
+
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if grid[i][j] == '*':
+                total += get_sum_of_gear_ratios(grid, i, j)
+
+    return total
+
+
+grid = []
+
+with open('input.txt', 'r', encoding='utf-8') as fin:
     for line in fin:
-        pass
+        grid.append(line.strip())
+
+print(get_total_gear_ratio(grid))
